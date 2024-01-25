0d2068afc8f5a81227bd7b809597a96b2914266e
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-01-25 22:07:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,5 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
-git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
