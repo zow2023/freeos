f62dfb1336ffc7cdfa7fb14b0990bc5d611fd501
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-06-15 13:30:30 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,14 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
 git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
-#git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus
+git clone https://github.com/kingyond/luci-app-accesscontrol-plus package/luci-app-accesscontrol-plus 
 #git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
 
 rm -rf feeds/packages/net/{xray-core,chinadns-ng,hysteria,v2ray-plugin}
 
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci 
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci  
 #git clone https://github.com/xiaorouji/openwrt-passwall.git -b  luci-smartdns-dev package/passwall/luci
 #git clone https://github.com/fw876/helloworld.git -b master package/helloworld
 rm -rf package/helloworld
